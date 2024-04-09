bc60f4b57ffdd2c8f67c861b776dc6b4d8b51a9a
shyam130701 <shyam130701@gmail.com>
2024-04-10 01:01:46 +0530

    codeql commit -14

--- src/main/java/com/authservice/auth/util/ValueMapper.java
+++ src/main/java/com/authservice/auth/util/ValueMapper.java
@@ -1,21 +1,22 @@
-//package com.authservice.auth.util;
-//
-//
-//import com.authservice.auth.dao.AuthRequest;
-//
-//public class ValueMapper {
-//
-//
-//    public static UserData convertDtoToEntity(AuthRequest userRequest)
-//    {
-//       UserData userData=new UserData();
-//       userData.setName(userRequest.getName());
-//       userData.setEmail(userRequest.getEmail());
-//       userData.setPassword(userRequest.getPassword());
-//       userData.setAge(userRequest.getAge());
-//       return userData;
-//    }
-//
-//
-//
-//}
+package com.authservice.auth.util;
+
+
+import com.authservice.auth.dao.AuthRequest;
+import com.authservice.auth.model.UserData;
+
+public class ValueMapper {
+
+
+    public static UserData convertDtoToEntity(AuthRequest userRequest)
+    {
+       UserData userData=new UserData();
+       userData.setName(userRequest.getName());
+       userData.setEmail(userRequest.getEmail());
+       userData.setPassword(userRequest.getPassword());
+       userData.setAge(userRequest.getAge());
+       return userData;
+    }
+
+
+
+}
